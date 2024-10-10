721a78591d3df65df0dda892f83e9ae1e3b76b61
superwhiskers <whiskerdev@protonmail.com>
2024-10-09 22:15:46 -0500

    reedbed: refactor absorbing layers and abstract over them in preparation for supporting multiple

--- lib/src/greens.rs
+++ lib/src/greens.rs
@@ -4,10 +4,23 @@ use rug::{float::Special, Float};
 
 use crate::utilities;
 
-/// Configuration structure for [`fn@large_beam_absorbing_layer`]
+/// An abstraction over the various `*AbsorbingLayer` structures
+pub trait AbsorbingLayer<'a> {
+    /// Get the location at which this layer starts
+    ///
+    /// This is usually the `z0` parameter on the individual structures
+    fn starts_at(&self) -> &'a Float;
+
+    /// Evaluate the absorbing layer using the specified parameters
+    ///
+    /// Not all implementations of [`trait@AbsorbingLayer`] will use all
+    /// parameters
+    fn evaluate_at(&self, z: &Float, r: &Float, tp: &Float) -> Float;
+}
+
 //TODO: document these parameters better
 #[derive(Clone, PartialEq)]
-pub struct LargeBeamAbsorbingLayerConfig<'a> {
+pub struct LargeBeamAbsorbingLayer<'a> {
     /// Units: cm^-1
     pub mu_a: &'a Float,
 
@@ -34,82 +47,85 @@ pub struct LargeBeamAbsorbingLayerConfig<'a> {
     pub precision: u64,
 }
 
-pub fn large_beam_absorbing_layer(
-    LargeBeamAbsorbingLayerConfig {
-        mu_a,
-        rho,
-        c,
-        k,
-        d,
-        z0,
-        e0,
-        precision,
-    }: LargeBeamAbsorbingLayerConfig,
-    z: &Float,
-    tp: &Float,
-) -> Float {
-    //TODO: make this less naive
-
-    let mut alpha = Float::with_val_64(precision, k);
-    alpha /= rho;
-    alpha /= c;
-
-    let mut term_1 = Float::with_val_64(precision, mu_a);
-    term_1 *= e0;
-    term_1 /= rho;
-    term_1 /= c;
-    term_1 /= 2.0;
-
-    let mut term_2 = Float::with_val_64(precision, z);
-    term_2 -= z0;
-    term_2 *= mu_a;
-    term_2 *= -1;
-    term_2.exp_mut();
-
-    if *tp == 0 {
-        return term_1 * term_2;
+impl<'a> AbsorbingLayer<'a> for LargeBeamAbsorbingLayer<'a> {
+    fn starts_at(&self) -> &'a Float {
+        self.z0
     }
 
-    let mut term_3 = Float::with_val_64(precision, mu_a);
-    term_3.square_mut();
-    term_3 *= tp;
-    term_3 *= &alpha;
-    term_3.exp_mut();
+    fn evaluate_at(&self, z: &Float, _r: &Float, tp: &Float) -> Float {
+        let LargeBeamAbsorbingLayer {
+            mu_a,
+            rho,
+            c,
+            k,
+            d,
+            z0,
+            e0,
+            precision,
+        } = *self;
 
-    let mut reciprocal_sqrt = Float::with_val_64(precision, &alpha);
-    reciprocal_sqrt *= tp;
-    reciprocal_sqrt *= 4.0;
-    reciprocal_sqrt.sqrt_mut();
-    reciprocal_sqrt.recip_mut();
+        //TODO: make this less naive
 
-    let mut sqrt_mu_a = alpha;
-    sqrt_mu_a *= tp;
-    sqrt_mu_a.sqrt_mut();
-    sqrt_mu_a *= mu_a;
+        let mut alpha = Float::with_val_64(precision, k);
+        alpha /= rho;
+        alpha /= c;
 
-    let mut argument_1 = Float::with_val_64(precision, z0);
-    argument_1 += d;
-    argument_1 -= z;
-    argument_1 *= &reciprocal_sqrt;
-    argument_1 += &sqrt_mu_a;
-    argument_1.erf_mut();
+        let mut term_1 = Float::with_val_64(precision, mu_a);
+        term_1 *= e0;
+        term_1 /= rho;
+        term_1 /= c;
+        term_1 /= 2.0;
 
-    let mut argument_2 = Float::with_val_64(precision, z0);
-    argument_2 -= z;
-    argument_2 *= &reciprocal_sqrt;
-    argument_2 += &sqrt_mu_a;
-    argument_2.erf_mut();
+        let mut term_2 = Float::with_val_64(precision, z);
+        term_2 -= z0;
+        term_2 *= mu_a;
+        term_2 *= -1;
+        term_2.exp_mut();
 
-    let mut term_4 = argument_1;
-    term_4 -= argument_2;
+        if *tp == 0 {
+            return term_1 * term_2;
+        }
 
-    term_1 * term_2 * term_3 * term_4
+        let mut term_3 = Float::with_val_64(precision, mu_a);
+        term_3.square_mut();
+        term_3 *= tp;
+        term_3 *= &alpha;
+        term_3.exp_mut();
+
+        let mut reciprocal_sqrt = Float::with_val_64(precision, &alpha);
+        reciprocal_sqrt *= tp;
+        reciprocal_sqrt *= 4.0;
+        reciprocal_sqrt.sqrt_mut();
+        reciprocal_sqrt.recip_mut();
+
+        let mut sqrt_mu_a = alpha;
+        sqrt_mu_a *= tp;
+        sqrt_mu_a.sqrt_mut();
+        sqrt_mu_a *= mu_a;
+
+        let mut argument_1 = Float::with_val_64(precision, z0);
+        argument_1 += d;
+        argument_1 -= z;
+        argument_1 *= &reciprocal_sqrt;
+        argument_1 += &sqrt_mu_a;
+        argument_1.erf_mut();
+
+        let mut argument_2 = Float::with_val_64(precision, z0);
+        argument_2 -= z;
+        argument_2 *= &reciprocal_sqrt;
+        argument_2 += &sqrt_mu_a;
+        argument_2.erf_mut();
+
+        let mut term_4 = argument_1;
+        term_4 -= argument_2;
+
+        term_1 * term_2 * term_3 * term_4
+    }
 }
 
-/// Configuration structure for [`fn@flat_top_beam_absorbing_layer`]
-//TODO: ditto for the configuration structure above
+//TODO: same todo as above
 #[derive(Clone, PartialEq)]
-pub struct FlatTopBeamAbsorbingLayerConfig<'a> {
+pub struct FlatTopBeamAbsorbingLayer<'a> {
     /// Units: cm^-1
     pub mu_a: &'a Float,
 
@@ -139,28 +155,29 @@ pub struct FlatTopBeamAbsorbingLayerConfig<'a> {
     pub precision: u64,
 }
 
-pub fn flat_top_beam_absorbing_layer(
-    FlatTopBeamAbsorbingLayerConfig {
-        mu_a,
-        rho,
-        c,
-        k,
-        d,
-        z0,
-        e0,
-        radius,
-        precision,
-    }: FlatTopBeamAbsorbingLayerConfig,
-    z: &Float,
-    r: &Float,
-    tp: &Float,
-) -> Float {
-    if *tp == 0 && r > radius {
-        return Float::with_val_64(precision, Special::Zero);
+impl<'a> AbsorbingLayer<'a> for FlatTopBeamAbsorbingLayer<'a> {
+    fn starts_at(&self) -> &'a Float {
+        self.z0
     }
 
-    let z_factor = large_beam_absorbing_layer(
-        LargeBeamAbsorbingLayerConfig {
+    fn evaluate_at(&self, z: &Float, r: &Float, tp: &Float) -> Float {
+        let FlatTopBeamAbsorbingLayer {
+            mu_a,
+            rho,
+            c,
+            k,
+            d,
+            z0,
+            e0,
+            radius,
+            precision,
+        } = *self;
+
+        if *tp == 0 && r > radius {
+            return Float::with_val_64(precision, Special::Zero);
+        }
+
+        let z_factor = LargeBeamAbsorbingLayer {
             mu_a,
             rho,
             c,
@@ -169,48 +186,47 @@ pub fn flat_top_beam_absorbing_layer(
             z0,
             e0,
             precision,
-        },
-        z,
-        tp,
-    );
-
-    if *tp == 0 {
-        return z_factor;
-    }
-
-    //TODO: don't duplicate this between large_beam_absorbing_layer and this
-    //      function
-    let mut alpha = Float::with_val_64(precision, k);
-    alpha /= rho;
-    alpha /= c;
-
-    z_factor
-        * if *r == 0 {
-            let mut r_factor = Float::with_val_64(precision, radius);
-            r_factor.square_mut();
-            r_factor /= -4.0;
-            r_factor /= alpha;
-            r_factor /= tp;
-            r_factor.exp_mut();
-            r_factor = 1 - r_factor;
-            r_factor
-        } else {
-            //TODO: this is not accurate at all. fix the marcum-q function
-            //      implementation
-
-            let mut a = Float::with_val_64(precision, 2.0);
-            a *= alpha;
-            a *= tp;
-            a.recip_mut();
-
-            let mut b = a.clone();
-            b *= radius;
-            a *= r;
-
-            let mut r_factor = utilities::marcum_q(1, &a, &b, precision);
-            r_factor = 1 - r_factor;
-            r_factor
         }
+        .evaluate_at(z, &Float::with_val_64(precision, Special::Zero), tp);
+
+        if *tp == 0 {
+            return z_factor;
+        }
+
+        //TODO: don't duplicate this between large_beam_absorbing_layer and
+        //      this function
+        let mut alpha = Float::with_val_64(precision, k);
+        alpha /= rho;
+        alpha /= c;
+
+        z_factor
+            * if *r == 0 {
+                let mut r_factor = Float::with_val_64(precision, radius);
+                r_factor.square_mut();
+                r_factor /= -4.0;
+                r_factor /= alpha;
+                r_factor /= tp;
+                r_factor.exp_mut();
+                r_factor = 1 - r_factor;
+                r_factor
+            } else {
+                //TODO: this is not accurate at all. fix the marcum-q function
+                //      implementation
+
+                let mut a = Float::with_val_64(precision, 2.0);
+                a *= alpha;
+                a *= tp;
+                a.recip_mut();
+
+                let mut b = a.clone();
+                b *= radius;
+                a *= r;
+
+                let mut r_factor = utilities::marcum_q(1, &a, &b, precision);
+                r_factor = 1 - r_factor;
+                r_factor
+            }
+    }
 }
 
 #[cfg(test)]
@@ -228,7 +244,7 @@ mod tests {
 
     #[test]
     fn large_beam_sanity() {
-        let config = LargeBeamAbsorbingLayerConfig {
+        let config = LargeBeamAbsorbingLayer {
             mu_a: &ONE,
             rho: &ONE,
             c: &ONE,
@@ -260,7 +276,7 @@ mod tests {
 
     #[test]
     fn flat_top_beam_sanity() {
-        let config = FlatTopBeamAbsorbingLayerConfig {
+        let config = FlatTopBeamAbsorbingLayer {
             mu_a: &ONE,
             rho: &ONE,
             c: &ONE,
